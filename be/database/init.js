172066d07b4e6d00f1ada6cfa689974f08d63c8c
datistpham <78075406+datistpham@users.noreply.github.com>
2023-10-16 20:55:37 +0700

    Update init.js

--- be/database/init.js
+++ be/database/init.js
@@ -2,7 +2,7 @@ import mysql from "mysql2"
 
 const connection= mysql.createPool({
     host: "185.232.14.52",
-    user: "u898129453_datistpham	",
+    user: "u898129453_datistpham",
     database: "u898129453_booking_ticket",
     password: "3Q+mkdHc",
     waitForConnections: true,
